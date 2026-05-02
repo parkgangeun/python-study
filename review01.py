ca2aeccf5589a50b0738ac6c941296ec4a6b6734
parkgangeun <parkeuneeee@gmail.com>
2026-05-02 23:49:16 +0900

    Day 2: 자료형(숫자)과 연산자

---
+++ review01.py
@@ -0,0 +1,13 @@
+print(7)
+print(3+10*2)
+age = 21
+print(age + 10)
+print(age + 18*2)
+daily_allowance = 5000
+print(daily_allowance * 26)
+print(daily_allowance + age + 10)
+print(daily_allowance + age + 10*2)
+print(10/3)
+print(10//3)
+print(10%3)
+print(5+"5")
