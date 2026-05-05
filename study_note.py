f716139d9a8327999dbc578c316898ee27d293d6
parkgangeun <parkeuneeee@gmail.com>
2026-05-05 23:47:25 +0900

    문자열 포맷팅 및 탈출 문자 내용 추가

--- study_note.py
+++ study_note.py
@@ -3,16 +3,16 @@
 # 1. 숫자 관련 함수 (math 모듈 필요)
 import math # 수학 함수용
 
-print(abs(-5))      # 절대값 (결과: 5)
-print(pow(4, 2))    # 제곱 (4의 2승 = 16)
-print(max(5, 12))   # 최댓값 (둘 중 큰 수)
-print(min(5, 12))   # 최솟값 (둘 중 작은 수)
-print(round(3.14))  # 반올림 (결과: 3)
+print(abs(-5))      # 절댓값 5
+print(pow(4, 2))    # 제곱 4^2 = 4*4 = 16
+print(max(5, 12))   # 최댓값 12
+print(min(5, 12))   # 최솟값 5
+print(round(3.14))  # 반올림 3
 
 # ★ 주의: 바닥(floor)과 천장(ceil)
-print(math.floor(4.99)) # 내림: 바닥으로 감 (4)
-print(math.ceil(3.14))  # 올림: 천장으로 감 (4)
-print(math.sqrt(16))    # 제곱근 (4.0)
+print(math.floor(4.99)) # 내림: 바닥으로 감 4
+print(math.ceil(3.14))  # 올림: 천장으로 감 4
+print(math.sqrt(16))    # 제곱근 4.0
 
 # 2. 랜덤 함수 (random 모듈 필요)
 import random
@@ -45,5 +45,25 @@ print(jumin[7:])     # 7부터 끝까지 (1234567)
 print(jumin[-7:])    # 뒤에서 7번째부터 끝까지
 
 # 6. 문자열 포맷팅 (f-string 추천)
-age = 25
-print(f"나는 {age}살입니다.") # 가장 편한 방법
+age = 20
+color = "빨간"
+# 방법 1: format 함수 사용 (인덱스 활용)
+print("나는 {0}색과 {1}색을 좋아해요.".format("파란", "빨간"))
+print("나는 {1}색과 {0}색을 좋아해요.".format("파란", "빨간")) # 순서 바꾸기 가능
+# 방법 2: f-string (가장 편리함, v3.6 이상)
+print(f"나는 {age}살이며, {color}색을 좋아해요.") # 변수 이름을 직접 넣음
+
+# 탈출 문자 (특수 기능을 하는 문자들)
+print("백문이 불여일견\n백견이 불여일타") # \n : 줄바꿈
+print('저는 "나도코딩"입니다.') # 문장 내 따옴표 포함법
+print("C:\\Users\\Desktop") # \\ : 문장 내에서 역슬래시(\) 자체를 표시
+
+# 문자열 퀴즈 핵심 로직 
+# 예: http://naver.com 에서 비밀번호 생성하기
+url = "http://naver.com"
+my_str = url.replace("http://", "") # 규칙 1: http:// 제거
+my_str = my_str[:my_str.index(".")] # 규칙 2: 처음 만나는 점(.) 전까지만 자르기
+
+# 규칙 3: 남은 글자 중 처음 3자리 + 총 글자 수 + 'e' 개수 + "!"
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print(f"{url}의 비밀번호는 {password}입니다.")
